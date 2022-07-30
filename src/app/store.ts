0235cdb068b0944fbdde81dc9e6b5fb6c2335a7d
yuktnk <yuki0wl@gmail.com>
2022-07-30 17:25:45 +0900

    Prettierをinstall、設定ファイルを作成し整形

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 import covidReducer from '../features/covid/covidSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    covid: covidReducer
+    covid: covidReducer,
   },
-});
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
--- src/features/covid/covidSlice.ts
+++ src/features/covid/covidSlice.ts
@@ -1,72 +1,78 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { RootState } from "../../app/store";
-import dataJson from "./data.json";
-import dataDailyJson from "./dataDaily.json";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { RootState } from '../../app/store'
+import dataJson from './data.json'
+import dataDailyJson from './dataDaily.json'
 
-const name = "covid";
-const apiUrl = "https://covid19.mathdro.id/api";
+const name = 'covid'
+const apiUrl = 'https://covid19.mathdro.id/api'
 
-type API_DATA = typeof dataJson;
-type API_DATA_DAILY = typeof dataDailyJson;
+type API_DATA = typeof dataJson
+type API_DATA_DAILY = typeof dataDailyJson
 
 type covidState = {
-    data: API_DATA;
-    country: string;
-    dailyData: API_DATA_DAILY;
-};
+  data: API_DATA
+  country: string
+  dailyData: API_DATA_DAILY
+}
 
 const initialState: covidState = {
-    data: dataJson,
-    country: "",
-    dailyData: dataDailyJson,
-};
+  data: dataJson,
+  country: '',
+  dailyData: dataDailyJson,
+}
 
 export const fetchAsyncGetTotal = createAsyncThunk(`${name}/get`, async () => {
-    const { data } = await axios.get<API_DATA>(apiUrl);
-    return data;
-});
+  const { data } = await axios.get<API_DATA>(apiUrl)
+  return data
+})
 
-export const fetchAsyncGetDaily = createAsyncThunk(`${name}/getDaily`, async () => {
-    const { data } = await axios.get<API_DATA_DAILY>(`${apiUrl}/daily`);
-    return data;
-});
+export const fetchAsyncGetDaily = createAsyncThunk(
+  `${name}/getDaily`,
+  async () => {
+    const { data } = await axios.get<API_DATA_DAILY>(`${apiUrl}/daily`)
+    return data
+  }
+)
 
-export const fetchAsyncGetCountry = createAsyncThunk(`${name}/getCountry`, async (country: string) => {
-    let dinamicUrl = apiUrl;
+export const fetchAsyncGetCountry = createAsyncThunk(
+  `${name}/getCountry`,
+  async (country: string) => {
+    let dinamicUrl = apiUrl
     if (country) {
-        dinamicUrl = `${dinamicUrl}/countries/${country}`;
+      dinamicUrl = `${dinamicUrl}/countries/${country}`
     }
-    const { data } = await axios.get<API_DATA>(dinamicUrl);
-    return { data, country };
-});
+    const { data } = await axios.get<API_DATA>(dinamicUrl)
+    return { data, country }
+  }
+)
 
 const covidSlice = createSlice({
-    name,
-    initialState,
-    reducers: {},
-    extraReducers: (builders) => {
-        builders.addCase(fetchAsyncGetTotal.fulfilled, (state, action) => {
-            return {
-                ...state,
-                data: action.payload,
-            };
-        });
-        builders.addCase(fetchAsyncGetDaily.fulfilled, (state, action) => {
-            return {
-                ...state,
-                dailyData: action.payload,
-            };
-        });
-        builders.addCase(fetchAsyncGetCountry.fulfilled, (state, action) => {
-            return {
-                ...state,
-                data: action.payload.data,
-                country: action.payload.country,
-            };
-        });
-    },
-});
+  name,
+  initialState,
+  reducers: {},
+  extraReducers: (builders) => {
+    builders.addCase(fetchAsyncGetTotal.fulfilled, (state, action) => {
+      return {
+        ...state,
+        data: action.payload,
+      }
+    })
+    builders.addCase(fetchAsyncGetDaily.fulfilled, (state, action) => {
+      return {
+        ...state,
+        dailyData: action.payload,
+      }
+    })
+    builders.addCase(fetchAsyncGetCountry.fulfilled, (state, action) => {
+      return {
+        ...state,
+        data: action.payload.data,
+        country: action.payload.country,
+      }
+    })
+  },
+})
 
 export const selectData = (state: RootState) => state.covid.data
 export const selectDailyData = (state: RootState) => state.covid.dailyData
