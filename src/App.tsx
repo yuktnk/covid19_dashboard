d9ffba57461f8ae503188f2eb33aad81491637ce
yuktnk <yuki0wl@gmail.com>
2022-07-31 13:38:24 +0900

    SwitchCountryコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Cards from './features/covid/cards/Cards'
 import Chart from './features/covid/chart/Chart'
 import PieChart from './features/covid/piechart/PieChart'
+import SwitchCountry from './features/covid/switchcountry/SwitchCountry'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Cards />
         <Chart />
         <PieChart />
+        <SwitchCountry />
       </header>
     </div>
   )
